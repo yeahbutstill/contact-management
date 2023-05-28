4791ba346c8fc13c8c50af1b92059879d75a1ea4
yeahbutstill <mayadani28@gmail.com>
2023-05-29 02:06:00 +0700

    setup

---
+++ src/main/java/com/yeahbutstill/restful/ContactManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeahbutstill/restful/ContactManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
