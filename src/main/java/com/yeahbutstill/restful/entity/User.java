53d4be0a298f10c75250de5713f64587a09b8e80
yeahbutstill <mayadani28@gmail.com>
2023-05-29 13:08:17 +0700

    user entity

---
+++ src/main/java/com/yeahbutstill/restful/entity/User.java
@@ -0,0 +1,38 @@
+package com.yeahbutstill.restful.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String username;
+
+    @NotBlank
+    @NotEmpty
+    private String password;
+
+    @NotBlank
+    @NotEmpty
+    private String name;
+
+    @Column(unique = true)
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+
+}
